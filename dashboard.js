23d7ee31c397c45aa0020c4861aff47807bec3d1
ruz-07 <rupeshkharel76@gmail.com>
2026-01-17 15:52:19 +0545

    Add: Lander page

---
+++ dashboard.js
@@ -0,0 +1,19 @@
+function servicepage(){
+    const service = document.getElementById("service").value;
+    const district = document.getElementById("district").value;
+
+    if(!service || !district){
+      alert("Please select both service and district");
+      return;
+    }
+
+    if(service === "citizenship"){
+      window.location.href = "citizenship.html";
+    }
+    else if(service.toLowerCase() === "nic"){
+      window.location.href = "nic.html";
+    }
+    else{
+      alert("Service page not yet available.");
+    }
+  }
